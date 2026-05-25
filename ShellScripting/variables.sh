67280856428b97168fd0e40d45db96e1d6fd23bf
VinithaSasidhar <ksasidharreddy789@gmail.com>
2026-05-25 16:55:18 +0530

    declaring simple variables

--- ShellScripting/variables.sh
+++ ShellScripting/variables.sh
@@ -1,12 +1,15 @@
 #!/bin/bash/
 
+# to execute any program in shell scripting : sh script.sh
+#ex: sh variables.sh
+
+# Hello world program in Shell Scripting.
+# echo in shell scripting is similar to print  pyhton/java/C language.
 echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
-echo "hello world"
+
+
+# how to declare variable
+name = "sasidhar"
+echo $name
+
+
